41917f718e804289beb7a12c9e4e384d379623bd
mingzezh <143566019+mingzezh@users.noreply.github.com>
2026-03-18 15:38:10 -0700

    fix(web): home page not rendering

--- server/web/app.py
+++ server/web/app.py
@@ -4,8 +4,17 @@ This module defines `app` and database for the frontend
 The database for the frontend is going to save the charts and equations
 """
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from starlette.responses import HTMLResponse
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="./server/web/static"), name="static")
+templates = Jinja2Templates(directory="./server/web/templates")
+
+
+# Serve the main dashboard page
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
