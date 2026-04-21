69e0327b6115a58b6187150c6cb921859e0ce5e4
wandering-dotcom <dvan.college073@gmail.com>
2026-04-21 10:16:33 -0700

    feat: db bridge and docker setup
    * added contents of mqtt_bridge folder to db
    
    * added docker-compose.yml to server
    
    * feat: added dockerfile for the project
    
    * fix: corrected variable names
    
    * fix: changed influxdb url back
    
    * fix: finished adding self.x to turn into instance variables
    
    * fix: change so have influx exists not false
    
    * feat: create README.md
    
    just a draft, really only has directions for influxdb setup
    
    * fix: specified env_file in docker-compose
    
    * add message for influx exists
    
    * fix: actually don't know how modified
    
    * added directions for mqtt setup
    
    * fix: should likely need to download Docker as well
    
    * modify directions
    
    * fix: now README.md
    
    * changed mqtt_host back to mosquitto
    
    * Revert "feat: webui new"
    
    This reverts commit b004dfa7d600ee76492ddbaddb0109c11f1783f0.
    
    * vim swp ignore
    
    * Reapply "feat: webui new"
    
    This reverts commit 2e2bb18a86fade7fbac5595015124d34e14f3a48.
    
    * removed swp file
    
    * ignored .env.docker
    
    ---------
    
    Co-authored-by: mingzezh <143566019+mingzezh@users.noreply.github.com>

--- server/__main__.py
+++ server/__main__.py
@@ -1,8 +1,11 @@
 import asyncio
+import os
 
 from fastapi import WebSocket
 from server.web.app import app
 import uvicorn
+from dotenv import load_dotenv
+from server.db.bridge import Bridge
 
 
 @app.websocket("/ws")
@@ -21,6 +24,20 @@ async def start():
 
 
 def main():
+    load_dotenv()
+    influx_url = os.getenv("INFLUXDB_URL")
+    influx_token = os.getenv("INFLUXDB_TOKEN")
+    influx_database = os.getenv("INFLUXDB_DATABASE")
+    mqtt_host = os.getenv("MQTT_HOST")
+    mqtt_port = int(os.getenv("MQTT_PORT", 1883))
+    influx_exists = all([influx_url, influx_token, influx_database, mqtt_host])
+
+    if influx_exists:
+       bridge = Bridge(influx_url, influx_token, influx_database, mqtt_host, mqtt_port)
+       bridge.start()
+
+    print(f"influx exists: {influx_exists}")
+
     try:
         asyncio.run(start())
     except KeyboardInterrupt:
@@ -29,3 +46,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# vim: et:sw=4
