ad55689202d0e31a82320e7f251aa914b82b9a25
wandering-dotcom <dvan037@ucr.edu>
2026-05-01 16:44:32 -0700

    fix: datapool sends to influxdb

--- server/streaming/stream.py
+++ server/streaming/stream.py
@@ -21,6 +21,4 @@ def init_streaming(datapool: Datapool):
     except LJMError:
         logger.warn("Unable to open t7 labjack through ethernet")
 
-    
-    # lj_test = LabJackTest(datapool)
-    # lj_test.init()
+ 
